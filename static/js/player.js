06b8d6e20d51238f4f1ee982ba4392bcd0921720
sweetsummergone <66779873+sweetsummergone@users.noreply.github.com>
2021-03-01 00:17:26 +0200

    redirected to node js server

--- static/js/player.js
+++ static/js/player.js
@@ -9,7 +9,8 @@ window.onload = function() {
         const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
         const match = vid.match(regExp);
         vid = match && match[7].length === 11 ? match[7] : false;
-        audio.src = `http://localhost/play.php?id=${vid}`;
+        audio.src = `http://localhost:8000/play/${vid}`;
+        audio.crossOrigin = "anonymous";
         rythm.addRythm('color2', 'color', 0, 10, {
             from:[255,100,255],
             to: [100,200,250]
