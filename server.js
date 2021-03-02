819498b2e2450dde67e1ee9b5de1a209c1ff3e0a
sweetsummergone <66779873+sweetsummergone@users.noreply.github.com>
2021-03-02 15:19:45 +0200

    Rebuild to client-side only Design update

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
-var express = require("express");
-var app = express();
+const {exec,spawn} = require("child_process");
+const express = require("express");
+const app = express();
 
 app.use(express.static(__dirname));
-
 app.use(express.static('static'));
 
-var server = app.listen(3000, '0.0.0.0', function() {
+const server = app.listen(3000, '0.0.0.0', function() {
     console.log('working on', server.address().port);
+});
+
+app.get('/play/:vid', function(req, res) {
+    const url = 'https://www.youtube.com/watch?v='+req.param("vid");
+    res.setHeader("Content-Type", "video/mp4");
+    exec(`youtube-dl.exe -f 140 --get-url ${url}`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+
+        const curl = spawn('curl.exe', [stdout.trim(), '-L', '--output', '-']);
+
+        curl.stdout.on('data', (data) => {
+            res.write(data);
+        });
+
+        curl.on('close', (code) => {
+            res.end();
+        });
+
+    });
 });
--- static/js/player.js
+++ static/js/player.js
@@ -1,24 +1,53 @@
-window.onload = function() {
-    let audio = document.getElementById('player');
-    let rythm = new Rythm();
-    const button = document.getElementById('play');
+document.onreadystatechange = function () {
+    let state = document.readyState;
+    if (state === 'interactive') {
+        document.getElementById('contents').style.visibility="hidden";
+    }
+    else if (state === "complete") {
+        document.getElementById('load').style.visibility="hidden";
+        document.getElementById('contents').style.visibility="visible";
 
-    button.addEventListener('click', function(e) {
-        let vid=document.getElementById('vinput').value
+        const buttonPlay = document.getElementById('play');
+        const buttonPause = document.getElementById('pause');
+        const audio = document.getElementById('player');
 
-        const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-        const match = vid.match(regExp);
-        vid = match && match[7].length === 11 ? match[7] : false;
-        audio.src = `http://localhost:8000/play/${vid}`;
-        audio.crossOrigin = "anonymous";
-        rythm.addRythm('color2', 'color', 0, 10, {
-            from:[255,100,255],
-            to: [100,200,250]
-        })
-        rythm.connectExternalAudioElement(audio);
-        rythm.setMusic(audio.src);
-        rythm.start();
-    });
+        let vid = document.getElementById('vinput').value;
+        let rythm = null;
+        document.querySelector('.color2').style.background = 'rgb(100, 200, 250)';
 
+        buttonPlay.addEventListener('click', function (e) {
+            if (rythm == null)
+                rythm = new Rythm()
+
+            rythm.connectExternalAudioElement(audio)
+            if(document.getElementById('vinput').value !== vid) {
+                vid = document.getElementById('vinput').value;
+
+                const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+                const match = vid.match(regExp);
+                vid = match && match[7].length === 11 ? match[7] : false;
+
+                audio.src = `http://localhost:3000/play/${vid}`;
+                audio.volume = 0.7;
+            }
+
+
+            rythm.addRythm('color2', 'color', 0, 10, {
+                from: [220, 39, 106],
+                to: [100, 200, 250]
+            })
+
+            audio.play();
+            rythm.start();
+            buttonPlay.hidden = true;
+            buttonPause.hidden = false;
+        });
+
+        buttonPause.addEventListener('click', function (e) {
+           audio.pause();
+           buttonPause.hidden = true;
+           buttonPlay.hidden = false;
+        });
+    }
 }
 
